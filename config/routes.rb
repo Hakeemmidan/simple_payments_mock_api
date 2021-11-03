f55fb1ae14b132aad221572ee602998fa80d5338
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2021-11-03 04:47:17 +0300

    Write integration tests for 'GET /accounts/{id}/money_send_transactions' and 'GET /accounts/{id}/money_receive_transactions'

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     scope 'v1.0', module: 'v1_0', as: 'v1_0' do
-      resources :accounts, only: %i[show create]
+      resources :accounts, only: %i[show create] do
+        get 'money_send_transactions'
+        get 'money_receive_transactions'
+      end
     end
   end
 end
--- spec/requests/api/v1.0/accounts_controller_spec.rb
+++ spec/requests/api/v1.0/accounts_controller_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1_0::AccountsControllers', type: :request do
-  let(:account) { create(:account, :verified) }
+  let(:sender) { create(:account, :verified) }
+  let(:receiver) { create(:account, :verified) }
+  let(:money_send_transaction) { create(:money_send_transaction, sender: account, receiver: receiver) }
 
   describe 'POST /accounts' do
     context 'when all required params are all available' do
       before do
         post '/api/v1.0/accounts', params: {
-          first_name: account.first_name,
-          last_name: account.last_name,
-          email: account.email,
-          phone_number: account.phone_number
+          first_name: sender.first_name,
+          last_name: sender.last_name,
+          email: sender.email,
+          phone_number: sender.phone_number
         }
       end
 
@@ -20,14 +22,14 @@ RSpec.describe 'Api::V1_0::AccountsControllers', type: :request do
 
       it 'returns the correct model object' do
         parsed_response_body = JSON.parse(response.body)
-        expect(parsed_response_body['email']).to eq(account.email)
-        expect(parsed_response_body['phone_number']).to eq(account.phone_number)
+        expect(parsed_response_body['email']).to eq(sender.email)
+        expect(parsed_response_body['phone_number']).to eq(sender.phone_number)
       end
     end
 
     context 'when missing required params' do
       before do
-        post '/api/v1.0/accounts', params: { first_name: account.first_name }
+        post '/api/v1.0/accounts', params: { first_name: sender.first_name }
       end
 
       it 'returns 422 status code' do
@@ -44,7 +46,7 @@ RSpec.describe 'Api::V1_0::AccountsControllers', type: :request do
   describe 'GET /accounts/{id}' do
     context 'when ID is valid' do
       before do
-        get "/api/v1.0/accounts/#{account.id}"
+        get "/api/v1.0/accounts/#{sender.id}"
       end
 
       it 'returns 200 status code' do
@@ -53,14 +55,78 @@ RSpec.describe 'Api::V1_0::AccountsControllers', type: :request do
 
       it 'returns the correct model object' do
         parsed_response_body = JSON.parse(response.body)
-        expect(parsed_response_body['email']).to eq(account.email)
-        expect(parsed_response_body['phone_number']).to eq(account.phone_number)
+        expect(parsed_response_body['email']).to eq(sender.email)
+        expect(parsed_response_body['phone_number']).to eq(sender.phone_number)
       end
     end
 
     context 'when ID is invalid' do
       before do
         get "/api/v1.0/accounts/id-that-doesn't-exist"
+      end
+
+      it 'returns 404 status code' do
+        expect(response.status).to eq(404)
+      end
+
+      it 'returns the correct error object shape' do
+        parsed_response_body = JSON.parse(response.body)
+        expect(parsed_response_body['errors']).to be_an_instance_of(Array)
+      end
+    end
+  end
+
+  describe 'GET /accounts/{id}/money_send_transactions' do
+    context 'when ID is valid' do
+      before do
+        get "/api/v1.0/accounts/#{account.id}/money_send_transactions"
+      end
+
+      it 'returns 200 status code' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the correct model objects' do
+        parsed_response_body = JSON.parse(response.body)['data']
+        expect(parsed_response_body[0]['status']).to eq(money_send_transaction.status)
+      end
+    end
+
+    context 'when ID is invalid' do
+      before do
+        get "/api/v1.0/accounts/id-that-doesn't-exist/money_send_transactions"
+      end
+
+      it 'returns 404 status code' do
+        expect(response.status).to eq(404)
+      end
+
+      it 'returns the correct error object shape' do
+        parsed_response_body = JSON.parse(response.body)
+        expect(parsed_response_body['errors']).to be_an_instance_of(Array)
+      end
+    end
+  end
+
+  describe 'GET /accounts/{id}/money_receive_transactions' do
+    context 'when ID is valid' do
+      before do
+        get "/api/v1.0/accounts/#{account.id}/money_receive_transactions"
+      end
+
+      it 'returns 200 status code' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the correct model objects' do
+        parsed_response_body = JSON.parse(response.body)['data']
+        expect(parsed_response_body).to be_empty
+      end
+    end
+
+    context 'when ID is invalid' do
+      before do
+        get "/api/v1.0/accounts/id-that-doesn't-exist/money_receive_transactions"
       end
 
       it 'returns 404 status code' do
